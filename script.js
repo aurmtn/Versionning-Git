c2877bac21d9146edef3a426d8977b93faaa7f44
Aurélien <aurmtn@gmail.com>
2023-04-04 10:00:05 +0200

    AJout de la balise script pour link le JS au HTML

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnToggle = document.getElementById('btn-toggle');
 const body = document.body;
 
 btnToggle.addEventListener('click', function() {
+  
   body.classList.toggle('dark-mode');
   if (body.classList.contains('dark-mode')) {
     btnToggle.textContent = 'Désactiver le mode sombre';
