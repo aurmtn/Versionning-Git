9d4a2f67a55e0036eb4ef2db938c08ab5c56ff59
Aurélien <aurmtn@gmail.com>
2023-04-04 10:18:52 +0200

    Merge branch 'main' of https://github.com/aurmtn/Versionning-Git

--- script.js
+++ script.js
@@ -13,8 +13,24 @@ btnToggle.addEventListener('click', function() {
 const btnLink = document.getElementById('btn-link');
 
 btnLink.addEventListener('click', () => {
-  alert('Vous allez être redirigé vers le lien !');
-  window.location.href = 'https://www.example.com';
+  alert('Vous allez être redirigé vers le lien du repo git !');
+  window.location.href = 'https://github.com/aurmtn/Versionning-Git';
+});
+
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault(); // empêche l'envoi du formulaire
+  const nom = document.getElementById('nom').value;
+  const prenom = document.getElementById('prenom').value;
+  const email = document.getElementById('email').value;
+  const telephone = document.getElementById('telephone').value;
+  if (nom === '' || prenom === '' || email === '' || telephone === '') {
+    alert('Veuillez remplir tous les champs du formulaire !');
+  } else {
+    alert('Merci ! Votre formulaire a bien été envoyé.');
+    contactForm.reset(); // réinitialise le formulaire
+  }
 });
 
 
